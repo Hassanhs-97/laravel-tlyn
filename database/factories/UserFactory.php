d70fc4cf97b41bf2d101faf0965bb325acfc23a2
hassanhs97 <hassansedighian97@gmail.com>
2025-04-13 09:25:07 +0330

    user factory and seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'name'           => 'اکبر',
+            'email'          => fake()->unique()->safeEmail(),
+            'password'       => bcrypt('password'),
             'remember_token' => Str::random(10),
+            'gold_balance'   => round(fake()->randomFloat(3, 0.1, 20), 3),
         ];
     }
 
